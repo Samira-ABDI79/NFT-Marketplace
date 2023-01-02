6fd6a92eb455a31ab5bb6379223058d2601af1a8
Samira-ABDI79 <samirabdiurmia@gmail.com>
2023-01-02 04:50:05 -0800

    feat (pages) : add Author

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import MainLayout from "../components/Layout/MainLayout";
 import { AppProps } from "../node_modules/next/app";
 import "../styles/globals.css";
-import "../styles/UIstyle.css"
 import "../styles/drawer.css"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
